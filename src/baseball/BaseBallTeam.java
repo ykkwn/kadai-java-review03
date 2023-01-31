43b65c090ce3245581f4bfdb3edb0b397df1da84
ykkwn <monmon_ranma@yahoo.co.jp>
2023-01-31 14:12:59 +0900

    Lesson5の課題作成

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,34 @@
+package baseball;
+
+public class BaseBallTeam {
+    //フィールド
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    //引数のないコンストラクタ
+    public BaseBallTeam() {
+    }
+
+    //引数のあるコンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    //gerRate()メソッド
+    public double getRate() {
+        double rate = (double)win /(this.win + this.lose);
+        return rate;
+    }
+
+    //report()メソッド
+    public void report() {
+        System.out.println(this.name + "の2022年の成績は " + this.win + "勝 " + this.lose + "敗　" + this.draw + "分、勝率は " + getRate() + "です。");
+    }
+
+ }
+
---
+++ src/baseball/Review03.java
@@ -0,0 +1,40 @@
+package baseball;
+
+public class Review03 {
+
+    public static void main(String[] args) {
+        
+//        BaseBallTeam team1 = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
+//        team1.report();
+//        
+//        BaseBallTeam team2 = new BaseBallTeam("横浜ベイスターズ", 73, 68, 2);
+//        team2.report();
+//        
+//        BaseBallTeam team3 = new BaseBallTeam("阪神タイガース", 68, 71, 4);
+//        team3.report();
+//        
+//        BaseBallTeam team4 = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
+//        team4.report();
+//        
+//        BaseBallTeam team5 = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
+//        team5.report();
+//        
+//        BaseBallTeam team6 = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);
+//        team6.report();
+            
+        BaseBallTeam teams[] = {
+            new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4),
+            new BaseBallTeam("横浜ベイスターズ", 73, 68, 2),
+            new BaseBallTeam("阪神タイガース", 68, 71, 4),
+            new BaseBallTeam("読売ジャイアンツ", 68, 72, 3),
+            new BaseBallTeam("広島東洋カープ", 66, 74, 3),
+            new BaseBallTeam("中日ドラゴンズ", 66, 75, 2)
+        };
+        
+        for (BaseBallTeam team : teams) {
+            team.report();
+        }
+        
+    }
+
+}
